fdaedff5a348c4ec1e72465d736d9c6f656ef25a
Michael Minella <mminella@pivotal.io>
2019-05-23 09:23:34 -0500

    Initial commit

---
+++ src/main/java/io/spring/batch/singlestepjobexample/SinglestepjobexampleApplication.java
@@ -0,0 +1,14 @@
+package io.spring.batch.singlestepjobexample;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SinglestepjobexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglestepjobexampleApplication.class, args);
+	}
+}
